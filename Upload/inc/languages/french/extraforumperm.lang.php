e95286ecbfd101e2341d3dd41f3210eea20b617f
AnoBug <crazycat@c-p-f.org>
2025-02-08 17:14:30 +0100

    French translation

---
+++ Upload/inc/languages/french/extraforumperm.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Translator: CrazyCat
+ * E-mail: crazycat@c-p-f.org
+ * Website: https://zeolia.chat
+ * Last updated: 2025-02-08 (yyyy-mm-dd)
+ */
+
+$l['extraforumperm'] = 'Extra Forum Permissions';
+$l['error_canrateownthread'] = 'Vous n\'êtes pas autorisé à noter votre propre sujet.';
+$l['error_canpostlinks'] = 'Vous n\'êtes pas autorisé à poster des liens sur ce forum.';
+$l['error_canpostimages'] = 'Vous n\'êtes pas autorisé à poster des images sur ce forum.';
+$l['error_canpostvideos'] = 'Vous n\'êtes pas autorisé à poster des vidéos sur ce forum.';
+
+$l['error_hidden_link'] = 'Vous n\'êtes pas autorisé à voir les liens sur ce forum.';
