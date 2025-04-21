744fd657bdec26fedcbdc07ab7b637a72d40503e
Sama34 <Sama34@users.noreply.github.com>
2025-04-21 02:23:15 -0700

    Update French files

--- Upload/inc/languages/french/admin/extraforumperm.lang.php
+++ Upload/inc/languages/french/admin/extraforumperm.lang.php
@@ -6,22 +6,36 @@
  * Last updated: 2025-02-08 (yyyy-mm-dd)
  */
 
-$l['extraforumperm'] = 'Extra Forum Permissions';
-$l['extraforumperm_description'] = 'Ajoute des permissions supplémentaires à votre forum.';
+$l = [
+    'extraforumperm' => 'Extra Forum Permissions',
+    'extraforumperm_description' => 'Ajoute des permissions supplémentaires à votre forum.',
 
-$l['group_extra'] = 'Extra';
+    'group_extra' => 'Extra',
 
-$l['extra_field_can_rate_own_threads'] = 'Peut noter ses propres sujets ?';
-$l['extra_field_can_moderate_own_threads'] = 'Peut modérer ses propres sujets ?';
-$l['extra_field_canstickyownthreads'] = 'Peut épingler ses propres sujets ?';
-$l['extra_field_cancloseownthreads'] = 'Peut fermer ses propres sujets ?';
-$l['extra_field_can_post_links_in_threads'] = 'Peut poster des liens dans les sujets ?';
-$l['extra_field_can_post_links'] = 'Peut poster des liens dans les réponses ?';
-$l['extra_field_can_view_links'] = 'Peut voir les liens dans les sujets et réponses ?';
-$l['extra_field_canpostimages'] = 'Peut poster des images ?';
-$l['extra_field_canpostvideos'] = 'Peut poster des vidéos ?';
+    'extra_field_can_rate_own_threads' => 'Peut noter ses propres sujets ?',
+    'extra_field_can_moderate_own_threads' => 'Peut modérer ses propres sujets ?',
+    'extra_field_canstickyownthreads' => 'Peut épingler ses propres sujets ?',
+    'extra_field_cancloseownthreads' => 'Peut fermer ses propres sujets ?',
+    'extra_field_can_post_links_in_threads' => 'Peut poster des liens dans les sujets ?',
+    'extra_field_can_post_links' => 'Peut poster des liens dans les réponses ?',
+    'extra_field_can_view_links' => 'Peut voir les liens dans les sujets et réponses ?',
+    'extra_field_canpostimages' => 'Peut poster des images ?',
+    'extra_field_canpostvideos' => 'Peut poster des vidéos ?',
 
-$l['extra_field_extra_subject_length'] = 'Taille maxi du sujet';
-$l['extra_field_extra_subject_length_desc'] = 'Longueur maximale du sujet pour les utilisateurs de ce groupe.';
-$l['extra_field_extra_maximum_attachments'] = 'Pièces jointes max';
-$l['extra_field_extra_maximum_attachments_desc'] = 'Maximum de pièces qu\'un utilisateur peut joindre à un message. Mettez 0 pour utiliser le réglage général.';
+    'extra_field_extra_subject_length' => 'Taille maxi du sujet',
+    'extra_field_extra_subject_length_desc' => 'Longueur maximale du sujet pour les utilisateurs de ce groupe.',
+    'extra_field_extra_maximum_attachments' => 'Pièces jointes max',
+    'extra_field_extra_maximum_attachments_desc' => 'Maximum de pièces qu\'un utilisateur peut joindre à un message. Mettez 0 pour utiliser le réglage général.',
+    'extra_field_extra_maximum_threads' => 'Maximum Threads',
+    'extra_field_extra_maximum_threads_desc' => 'This is the total number of threads allowed per user per day. 0 for unlimited.',
+
+    'extra_forum_tab' => 'Extra Permissions',
+    'extra_maximum_threads' => 'Maximum Threads <br /><small class="input">This is the total number of threads allowed per user. 0 to fallback to group and custom permissions. If not 0, will overwrite group and custom permissions.</small><br />',
+    'extra_maximum_threads_type' => 'Maximum Threads Interval Type<br /><small class="input">This affects all three, group and custom permissions as well as the above forum setting, for this forum.</small><br />',
+    'extra_maximum_threads_type_hours' => 'Hours',
+    'extra_maximum_threads_type_days' => 'Days',
+    'extra_maximum_threads_type_weeks' => 'Weeks',
+    'extra_maximum_threads_type_amount' => 'Maximum Threads Interval Amount<br /><small class="input">This affects all three, group and custom permissions as well as the above forum setting, for this forum.</small><br />',
+
+    'extra_plugin_library' => 'This plugin requires <a href="{1}">PluginLibrary</a> version {2} or later to be uploaded to your forum.',
+];
--- Upload/inc/languages/french/extraforumperm.lang.php
+++ Upload/inc/languages/french/extraforumperm.lang.php
@@ -6,10 +6,20 @@
  * Last updated: 2025-02-08 (yyyy-mm-dd)
  */
 
-$l['extraforumperm'] = 'Extra Forum Permissions';
-$l['error_canrateownthread'] = 'Vous n\'êtes pas autorisé à noter votre propre sujet.';
-$l['error_canpostlinks'] = 'Vous n\'êtes pas autorisé à poster des liens sur ce forum.';
-$l['error_canpostimages'] = 'Vous n\'êtes pas autorisé à poster des images sur ce forum.';
-$l['error_canpostvideos'] = 'Vous n\'êtes pas autorisé à poster des vidéos sur ce forum.';
+$l = [
+    'extraforumperm' => 'Extra Forum Permissions',
+    'error_canrateownthread' => 'Vous n\'êtes pas autorisé à noter votre propre sujet.',
+    'error_canpostlinks' => 'Vous n\'êtes pas autorisé à poster des liens sur ce forum.',
+    'error_canpostimages' => 'Vous n\'êtes pas autorisé à poster des images sur ce forum.',
+    'error_canpostvideos' => 'Vous n\'êtes pas autorisé à poster des vidéos sur ce forum.',
+    'error_hidden_link' => 'Vous n\'êtes pas autorisé à voir les liens sur ce forum.',
 
-$l['error_hidden_link'] = 'Vous n\'êtes pas autorisé à voir les liens sur ce forum.';
+    'error_extra_maximum_threads_forum' => "I'm sorry, but your daily thread limit for this forum has been exceeded.",
+    'error_extra_maximum_threads_group' => "I'm sorry, but your daily thread limit has been exceeded.",
+    'error_extra_maximum_threads_forum_note' => ' Please wait till tomorrow to post further or contact your administrator.<br /><br />',
+    'error_extra_maximum_threads_forum_description' => ' The maximum amount of threads you may make in this forum is {1} every {2} {3}.',
+    'error_extra_maximum_threads_group_description' => ' The maximum amount of threads you may make is {1} every {2} {3}.',
+    'error_extra_maximum_threads_forum_hour' => 'hour(s)',
+    'error_extra_maximum_threads_forum_day' => 'day(s)',
+    'error_extra_maximum_threads_forum_week' => 'week(s)',
+];
